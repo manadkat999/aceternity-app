e9603da5369b28215af52ae1b28b968bbd014115
Manik Anadkat <manik.anadkat@outlook.com>
2025-08-21 20:01:39 +0100

    first commit

---
+++ components/typewriter.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+export function TypewriterEffectSmoothDemo() {
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "Aceternity.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <TypewriterEffectSmooth words={words} /> 
+  );
+}
